13d4a238c160cad13139fb5e366a40aa03b02576
Tania <rawrtan@gmail.com>
2015-01-30 13:57:42 -0800

    501 301 selection screen

--- app/src/main/java/me/taniad/dartswithfriends/GameSelectionActivity.java
+++ app/src/main/java/me/taniad/dartswithfriends/GameSelectionActivity.java
@@ -1,9 +1,10 @@
 package me.taniad.dartswithfriends;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 
 public class GameSelectionActivity extends ActionBarActivity {
@@ -13,28 +14,23 @@ public class GameSelectionActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_game_selection);
-    }
 
+        LinearLayout cricket = (LinearLayout) findViewById(R.id.cricket);
+        cricket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+            }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        LinearLayout game501301 = (LinearLayout) findViewById(R.id.game501301);
+        game501301.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent select501301 = new Intent(GameSelectionActivity.this, Select501301Activity.class);
+                startActivity(select501301);
+                overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+            }
+        });
     }
 }
---
+++ app/src/main/java/me/taniad/dartswithfriends/Select501301Activity.java
@@ -0,0 +1,16 @@
+package me.taniad.dartswithfriends;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class Select501301Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_501301_selection);
+    }
+
+}
