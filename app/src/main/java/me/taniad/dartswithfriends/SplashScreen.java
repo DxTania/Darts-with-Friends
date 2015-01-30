6304633ecf61b4b3777f1966069d1a78287425d0
Tania <rawrtan@gmail.com>
2015-01-30 12:57:12 -0800

    Splash screen transition

--- app/src/main/java/me/taniad/dartswithfriends/SplashScreen.java
+++ app/src/main/java/me/taniad/dartswithfriends/SplashScreen.java
@@ -1,24 +1,37 @@
 package me.taniad.dartswithfriends;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
-public class MainActivity extends ActionBarActivity {
+public class SplashScreen extends ActionBarActivity {
+    // Splash screen time out
+    private static final int TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent game = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(game);
+                overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out);
+            }
+        }, TIME_OUT);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
         return true;
     }
 
